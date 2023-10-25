51867a60067301e526b93d9ad324ce63ca7c6651
Alok Maheshwari <v-2alokm@hotmail.com>
2023-10-25 18:38:34 +0530

    key removed

--- lz01/backend.tf
+++ lz01/backend.tf
@@ -4,7 +4,6 @@ terraform {
     storage_account_name = "tfstatetmodemo"
     container_name       = "tfstate"
     key                  = "git/amgit.terraform.tfstate"
-    access_key = "uf3EnthUnHBF4NVZfj9L87FuRLaBjUdiecjtqGHaM5AVXiZhyX5O4bGX6mnHYcYLwYIBeBVo6LlQ+AStOlCfOw==" 
   }
   
 }
