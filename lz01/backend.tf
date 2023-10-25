f193a52149bcb9b5728246bde4c2436fe596e9d2
Alok Maheshwari <v-2alokm@hotmail.com>
2023-10-25 18:48:24 +0530

    SAS provided

--- lz01/backend.tf
+++ lz01/backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "tfstatetmodemo"
     container_name       = "tfstate"
     key                  = "git/amgit.terraform.tfstate"
+    sas_token = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-10-25T21:17:32Z&st=2023-10-25T13:17:32Z&spr=https&sig=opmgX5iTtne5Enw5gZhv1KAdn%2BvRizaAUhOblK5WWhk%3D"
   }
   
 }
