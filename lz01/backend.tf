f3ee847b1e2284980f773b71458f066e45e4b51e
Alok Maheshwari <alok.maheshwari@hcl.com>
2023-10-26 15:39:24 +0530

    Storage Account Key Auth with OIDC Provider

--- lz01/backend.tf
+++ lz01/backend.tf
@@ -4,7 +4,7 @@ terraform {
 #    storage_account_name = "abcd1234"
 #    container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    use_oidc             = true
+  #  use_oidc             = true
 #    subscription_id      = "00000000-0000-0000-0000-000000000000"
  #   tenant_id            = "00000000-0000-0000-0000-000000000000"
   }
