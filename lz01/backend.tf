4ee9f11856571599e7097ee0244533b8b7619ae2
Alok Maheshwari <alok.maheshwari@hcl.com>
2023-10-26 15:02:34 +0530

    provider block update

--- lz01/backend.tf
+++ lz01/backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "azurerm" {
-#     resource_group_name  = "Alok_Maheshwari_RG"
-#     storage_account_name = "tfstatetmodemo"
-#  #   container_name       = "tfstate"
-#     key                  = "git/amgit.terraform.tfstate"
-# #    sas_token = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-10-25T21:17:32Z&st=2023-10-25T13:17:32Z&spr=https&sig=opmgX5iTtne5Enw5gZhv1KAdn%2BvRizaAUhOblK5WWhk%3D"
- 
+#    resource_group_name  = "StorageAccount-ResourceGroup"
+#    storage_account_name = "abcd1234"
+#    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+    use_oidc             = true
+#    subscription_id      = "00000000-0000-0000-0000-000000000000"
+ #   tenant_id            = "00000000-0000-0000-0000-000000000000"
   }
-  
 }
