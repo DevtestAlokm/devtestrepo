45f42f07b25c9cf804c0921ad8040b3b97655537
Alok Maheshwari <alok.maheshwari@hcl.com>
2023-10-26 12:45:57 +0530

    tf backend comment

--- lz01/backend.tf
+++ lz01/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Alok_Maheshwari_RG"
-    storage_account_name = "tfstatetmodemo"
- #   container_name       = "tfstate"
-    key                  = "git/amgit.terraform.tfstate"
-#    sas_token = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-10-25T21:17:32Z&st=2023-10-25T13:17:32Z&spr=https&sig=opmgX5iTtne5Enw5gZhv1KAdn%2BvRizaAUhOblK5WWhk%3D"
+#     resource_group_name  = "Alok_Maheshwari_RG"
+#     storage_account_name = "tfstatetmodemo"
+#  #   container_name       = "tfstate"
+#     key                  = "git/amgit.terraform.tfstate"
+# #    sas_token = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-10-25T21:17:32Z&st=2023-10-25T13:17:32Z&spr=https&sig=opmgX5iTtne5Enw5gZhv1KAdn%2BvRizaAUhOblK5WWhk%3D"
+ 
   }
   
 }
